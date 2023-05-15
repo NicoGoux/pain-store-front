82bcc47b52ff3ef22e7554ab41f6398dba82faf0
Nicolas Goux <nicolasgoux2000@gmail.com>
2023-05-15 03:10:20 -0300

    mejorado el responsive del header

--- src/components/menu/navbar/Navbar.jsx
+++ src/components/menu/navbar/Navbar.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
+import { AsideButton } from '../../../assets/aside_button/AsideButton';
 
 function Navbar({ routes }) {
 	return (
-		<header className='relative flex h-40 items-center justify-between gap-8 w-full px-10 border-2 bg-card-background-color border-border-color shadow-header'>
-			<figure className='relative top-20 min-w-[150px] max-w-[200px] w-full'>
-				{/* <img className='w-full h-full' src='/logo.svg' alt='logo pain store' /> */}
+		<header className='relative flex h-40 items-center justify-between gap-10 w-full px-10 border-b-2 border-border-color sm:border-2 sm:bg-card-background-color sm:border-border-color sm:shadow-header'>
+			<div className='sm:hidden'>
+				<AsideButton isOpen={false} />
+			</div>
+			<figure className='relative sm:top-20 max-w-[150px] md:min-w-[150px] md:max-w-[200px] w-full'>
 				<img className='w-full h-full' src='/painLogo.png' alt='logo pain store' />
 			</figure>
-			<ul className='flex items-end w-3/5 h-full gap-4 list-none p-0 text-2xl font-medium'>
+
+			<ul className='hidden items-end h-full list-none p-0 text-md font-medium sm:flex md:text-2xl xl:w-3/5 transition-all'>
 				{/* TODO para maquetar */}
 				<li className='p-3 bg-primary-button-bg-color rounded-t-lg'>TIENDA</li>
 				<li className='p-3'>DESTACADO</li>
 				<li className='p-3'>CONTACTO</li>
 			</ul>
-			<div className='hidden flex-col text-2xl font-extrabold w-fit whitespace-nowrap mr-14 xl:flex'>
+			<div className='hidden flex-col text-2xl font-extrabold w-fit whitespace-nowrap xl:flex'>
 				<p>COMPRAMOS TUS</p>
 				<p className='text-4xl text-secondary-font-color secondary-text-shadow'>SKINS</p>
 				<p>AL MEJOR PRECIO</p>
 			</div>
 
-			<div className='flex flex-col gap-4 xl:flex-row xl:gap-6'>
+			<div className='hidden sm:flex flex-col gap-4 xl:flex-row xl:gap-6'>
 				<button className='primary-button w-32'>INGRESAR</button>
 				<button className='secondary-button w-32'>REGISTRARSE</button>
 			</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,22 @@ export default {
 			'primary-button-font-color': '#1a1a1a',
 			'secondary-button-font-color': '#f7931e',
 		},
+		screens: {
+			sm: '750px',
+			// => @media (min-width: 640px) { ... }
+
+			md: '875px',
+			// => @media (min-width: 768px) { ... }
+
+			lg: '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			xl: '1280px',
+			// => @media (min-width: 1280px) { ... }
+
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		},
 		extend: {},
 	},
 	plugins: [],
