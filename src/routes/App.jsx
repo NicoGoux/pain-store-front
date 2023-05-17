0f6fa99116b4c18517d2288492ef2cfafc2caa02
Nicolas Goux <nicolasgoux2000@gmail.com>
2023-05-17 07:42:29 -0300

    Cambio en router para hosteo en netlify

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Menu } from '../components/menu/Menu';
 import { Store } from './store/Store';
 
 function App() {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Menu />
 
 			<Routes>
 				<Route path='/' element={<Navigate to={'/store'} replace={true} />}></Route>
 				<Route path='/store' element={<Store />} />
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	);
 }
 
